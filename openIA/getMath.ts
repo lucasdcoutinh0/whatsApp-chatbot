db949d9d8fd7af85374f040a5524592c4b06c173
Lucas Coutinho <lucas.coutinho@eagronom.com>
2023-03-01 12:35:31 -0300

    Add support for math

---
+++ openIA/getMath.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+export const getMath = async (math: string) => {
+    try{
+        const response = await axios.get('http://api.wolframalpha.com/v2/query', {
+            params: {
+              input: math,
+              appid: apiKey,
+              output: 'json'
+            }
+          })
+        return response.data.queryresult.pods[1].subpods[0].plaintext;
+    }
+    catch(error: any){
+        console.error(error.response.data);
+    }
+}
